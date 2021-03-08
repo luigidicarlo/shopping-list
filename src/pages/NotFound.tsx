7cf519b2151d94beadaea4df669028a0bfd8c0d2
Luis Huerta <luis@ext.conviertemas.com>
2021-03-07 20:19:35 -0400

    First commit

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export function NotFound() {
+	return (
+		<div>
+			<h1>404 Not Found</h1>
+			<p>
+				It seems that the resource you tried to access is no longer available
+			</p>
+		</div>
+	);
+}
---
+++ src/pages/ShoppingList.tsx
@@ -0,0 +1,7 @@
+export function ShoppingList() {
+  return (
+    <div className="shopping-list">
+      <h1>Shopping List</h1>
+    </div>
+  )
+}
